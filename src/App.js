18f30687e6f4d36599cfa370d5dd3d0559f43643
Nikita Reshetnikov <nikita.reshetnikov9@gmail.com>
2021-08-31 11:52:11 +0300

    feat: add quiz component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
+
 function App() {
   return (
-    <div className="App">
-      <h1>React</h1>
-    </div>
+    <Layout>
+      <Quiz />
+    </Layout>
   );
 }
 
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Quiz.css";
+
+class Quiz extends Component {
+  render() {
+    return <h1>Quiz componenent</h1>;
+  }
+}
+
+export default Quiz;
