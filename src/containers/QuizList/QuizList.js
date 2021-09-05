2712cc44ff39cb7e0935c93ec228eb9877df291d
Nikita Reshetnikov <nikita.reshetnikov9@gmail.com>
2021-09-05 18:06:15 +0300

    feat: add get method to quizList component

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -4,14 +4,37 @@ import "./QuizList.css";
 import axios from "axios";
 
 export default class QuizList extends Component {
+  state = { quizes: [] };
+
   renderQuizes() {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map((quiz) => {
       return (
-        <li key={index}>
-          <NavLink to={"/quiz/" + quiz}>Тест {quiz}</NavLink>
+        <li key={quiz.id}>
+          <NavLink to={"/quiz/" + quiz.id}>{quiz.name}</NavLink>
         </li>
       );
     });
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://react-quiz-c09a9-default-rtdb.europe-west1.firebasedatabase.app/quizes.json"
+      );
+      const quizes = [];
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Тест № ${index + 1}`,
+        });
+      });
+
+      this.setState({
+        quizes,
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
